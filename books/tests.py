9855be67598dfe9a9dcf74c11b559c7228700e70
JzKrpts <261347858+JzKrpts@users.noreply.github.com>
2026-04-23 16:41:37 -0700

    Initial Commit

---
+++ books/tests.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Book
+
+
+class BookTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title="A Good Title",
+            subtitle="A Great Subtitle",
+            author="Tom Christie",
+            isbn="1234567890123",
+        )
+
+    def test_book_content(self):
+        self.assertEqual(self.book.title, "A Good Title")
+        self.assertEqual(self.book.subtitle, "A Great Subtitle")
+        self.assertEqual(self.book.author, "Tom Christie")
+        self.assertEqual(self.book.isbn, "1234567890123")
+
+    def test_book_listview(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response, "A Great Subtitle")
+        self.assertContains(response, "books/book_list.html")
