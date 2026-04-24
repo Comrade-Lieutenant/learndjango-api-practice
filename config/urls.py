23b26a5d0fedfac4f5483f0df95db50b0a38cde6
JzKrpts <261347858+JzKrpts@users.noreply.github.com>
2026-04-23 23:32:31 -0700

    feat: library api

--- config/urls.py
+++ config/urls.py
@@ -18,4 +18,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("books.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("api/", include("apis.urls")), path("", include("books.urls"))]
